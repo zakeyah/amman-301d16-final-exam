b9bdd36a408352b4113e7cef8b6ed68b5e8c8d37
zakeyah <mom11omar@gmail.com>
2021-04-20 12:59:35 +0300

    exam

---
+++ schema.sql
@@ -0,0 +1,12 @@
+
+
+DROP TABLE IF  EXISTS  simpson;
+
+CREATE TABLE simpson(
+  id SERIAL PRIMARY KEY NOT NULL,
+  quote VARCHAR(256) NOT NULL,
+  character VARCHAR(256) NOT NULL,
+  image VARCHAR(256) NOT NULL,
+  characterDirection VARCHAR(256) NOT NULL
+  
+);
--- server.js
+++ server.js
@@ -11,18 +11,24 @@ require('dotenv').config();
 
 // Application Setup
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 // Express middleware
 // Utilize ExpressJS functionality to parse the body of the request
+app.use(express.urlencoded({extended :true}))
 
 // Specify a directory for static resources
+app.use(express.static('public'))
 
 // define our method-override reference
+app.use(methodOverride('_method'))
+
 
 // Set the view engine for server-side templating
+app.set('view engine', 'ejs');
 
 // Use app cors
+app.use(cors())
 
 
 // Database Setup
@@ -30,10 +36,68 @@ const client = new pg.Client(process.env.DATABASE_URL);
 
 // app routes here
 // -- WRITE YOUR ROUTES HERE --
+app.get('/',renderHome)
+app.post('/favorite-quotes',handelSave)
+app.get('/saved',renderSaved)
+app.get('/favorite-quotes/:quote_id',renderDetails)
+app.delete('/favorite-quotes/:quote_id',handelDelete)
+app.put('/favorite-quotes/:quote_id',handelUpdate)
+
+
 
 
 // callback functions
 // -- WRITE YOUR CALLBACK FUNCTIONS FOR THE ROUTES HERE --
+function handelUpdate(req,res){
+    const id= req.params.quote_id
+    const sql= 'UPDATE simpson SET character=$1, WHERE id=$2'
+    client.query(sql,[id])
+    .then(()=>{
+        res.redirect(`/favorite-quotes/${id}`)
+    })
+}
+function handelDelete(req,res){
+    const id= req.params.quote_id
+    const sql='DELETE FROM simpson WHERE id=$1'
+    client.query(sql,[id])
+    .then(()=>{
+        res.redirect('/favorite-quotes')
+    })
+
+}
+function renderDetails(req,res){
+    const id= req.params.quote_id
+    const sql='SELECT * FROM simpson WHERE id=$1'
+    client.query(sql,[id])
+    .then(data=>{
+        res.render('details',{datbaseData:data.rows})
+    })
+}
+function renderSaved(req,res){
+    const sql='SELECT * FROM simpson'
+    client.query(sql)
+    .then(data=>{
+        res.render('seved',{datbaseData:data.rows})
+    })
+}
+
+function handelSave(req,res){
+    const data= req.body
+    const values= Object.values(data)
+    const sql='INSERT INTO simpson (quote,character,image,characterdirection)VALUES($1,$2,$3,$4)'
+    client.query(sql,values)
+    .then(()=> res.redirect('/saved'))
+}
+function renderHome (req,res){
+    const url='https://thesimpsonsquoteapi.glitch.me/quotes?count=10'
+    superagent.get(url)
+    .set('User-Agent', '1.0')
+    .then(data=>{
+        const array=data.body.map(result=>new Simp(result)) 
+        console.log(array)
+        res.render('home',{apiData:array})
+    })
+}
 
 // helper functions
 
@@ -41,3 +105,9 @@ const client = new pg.Client(process.env.DATABASE_URL);
 client.connect().then(() =>
     app.listen(PORT, () => console.log(`Listening on port: ${PORT}`))
 );
+function Simp (info){
+    this.quote = info.quote;
+    this.character = info.character;
+    this.image = info.image;
+    this.characterdirection=info.characterDirection
+}
